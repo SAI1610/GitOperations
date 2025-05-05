db58ade8376d3f494d8905568544bd1eeaf5ffa2
SAI1610 <vallemsai12801@gmail.com>
2025-05-05 13:04:33 +0530

    ME changed the app.java

--- Pratice_GIT/src/main/java/com/sai/App.java
+++ Pratice_GIT/src/main/java/com/sai/App.java
@@ -20,6 +20,7 @@ public class App {
 	public void divison()
 	{
 		System.out.println("divison!!!!");
+		System.out.println("jdbdjdd");
 	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
