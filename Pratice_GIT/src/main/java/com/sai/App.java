5ded9b5d6c8f23d23997881d079365a584939cbc
saipavanmalapati596 <saipavankumarmalapati@gmail.com>
2025-05-05 12:25:39 +0530

    sai changed to app.java

--- Pratice_GIT/src/main/java/com/sai/App.java
+++ Pratice_GIT/src/main/java/com/sai/App.java
@@ -13,6 +13,10 @@ public class App {
 	{
 		System.out.println("Hello world !!!!");
 	}
+	public void add2()
+	{
+		System.out.println("Hello world !!!!");
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
