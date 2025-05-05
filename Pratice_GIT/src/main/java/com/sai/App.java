afc57e4dc5a4f790f8686d157ced3093611f7101
saipavanmalapati596 <saipavankumarmalapati@gmail.com>
2025-05-05 11:54:34 +0530

    saipavan changed the app.java

--- Pratice_GIT/src/main/java/com/sai/App.java
+++ Pratice_GIT/src/main/java/com/sai/App.java
@@ -3,8 +3,14 @@ package com.sai;
 /**
  * Hello world!
  */
+
 public class App {
+	public void add()
+	{
+		System.out.println("Hello !!!!");
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
     }
 }
